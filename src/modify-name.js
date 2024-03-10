11a3db6cc536d430e43432245fdee63a3583c982
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-10 15:46:56 +0100

    Partial commit add to lowercase

--- src/modify-name.js
+++ src/modify-name.js
@@ -6,7 +6,12 @@ function get_str_consecutive_whitespaces_to_only_one(string) {
   return string.replace(/\s+/g, " ");
 }
 
+function get_str_lowercase(string) {
+  return string.toLowerCase();
+}
+
 export const exportedForTesting = {
   get_str_drop_lead_and_trail_whitespaces,
   get_str_consecutive_whitespaces_to_only_one,
+  get_str_lowercase,
 };
--- test/modify-name.test.js
+++ test/modify-name.test.js
@@ -8,12 +8,20 @@ describe("Check function get_str_drop_lead_and_trail_whitespaces", function () {
       exportedForTesting.get_str_drop_lead_and_trail_whitespaces(" ab c   "),
     ).toBe("ab c");
   });
+});
 
+describe("Check function get_str_consecutive_whitespaces_to_only_one", function () {
   it("Check expected result", function () {
     expect(
       exportedForTesting.get_str_consecutive_whitespaces_to_only_one(
         " a  b    c",
       ),
     ).toBe(" a b c");
+  });
+});
+
+describe("Check function get_str_lowercase", function () {
+  it("Check expected result", function () {
+    expect(exportedForTesting.get_str_lowercase("ÁáÑ")).toBe("áañ");
   });
 });
