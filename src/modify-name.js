9df338d6e3872f952c6a165018b93128cba9fbab
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-10 15:11:21 +0100

    Add replace consecutive whitespaces

--- src/modify-name.js
+++ src/modify-name.js
@@ -2,6 +2,11 @@ function get_str_drop_lead_and_trail_whitespaces(string) {
   return string.trim();
 }
 
+function get_str_consecutive_whitespaces_to_only_one(string) {
+  return string.replace(/\s+/g, " ");
+}
+
 export const exportedForTesting = {
   get_str_drop_lead_and_trail_whitespaces,
+  get_str_consecutive_whitespaces_to_only_one,
 };
--- test/modify-name.test.js
+++ test/modify-name.test.js
@@ -8,4 +8,12 @@ describe("Check function get_str_drop_lead_and_trail_whitespaces", function () {
       exportedForTesting.get_str_drop_lead_and_trail_whitespaces(" ab c   "),
     ).toBe("ab c");
   });
+
+  it("Check expected result", function () {
+    expect(
+      exportedForTesting.get_str_consecutive_whitespaces_to_only_one(
+        " a  b    c",
+      ),
+    ).toBe(" a b c");
+  });
 });
