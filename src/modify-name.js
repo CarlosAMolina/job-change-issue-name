486f6f3a834af8433a29bbb1fefed656a87e570b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-10 21:57:11 +0100

    Add code to connect front and back

--- src/modify-name.js
+++ src/modify-name.js
@@ -1,3 +1,38 @@
+document.getElementById("search-button").onclick = function () {
+  run_input_query();
+};
+
+document.getElementById("search-input").onkeydown = function (e) {
+  if (e.key === "Enter") {
+    run_input_query();
+  }
+};
+
+function run_input_query() {
+  const searchTerm = document.getElementById("search-input").value;
+  try {
+    document.getElementById("response-error-div").classList.add("hidden");
+    document.getElementById("response-div").classList.remove("hidden");
+    const html = getResultHtml(searchTerm);
+    document.getElementById("search-result").innerHTML = html;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("response-div").classList.add("hidden");
+    document.getElementById("response-error-div").classList.remove("hidden");
+    const errorMessage = getErrorMessageToShow(error);
+    document.getElementById("error-output").textContent = errorMessage;
+  }
+}
+
+function getResultHtml(searchTerm) {
+  const modified_string = getStrModified(searchTerm);
+  return "<p>".concat(modified_string, "</p>");
+}
+
+function getErrorMessageToShow(error) {
+  return "Error. ".concat(error.message);
+}
+
 function getStrModified(string) {
   let result = string;
   result = getStrDropCharactersInBrackets(result);
@@ -52,13 +87,14 @@ function getStrReplaceWhitespaces(string) {
   return string.replace(/\s/g, "-");
 }
 
-export const exportedForTesting = {
-  getStrModified,
-  getStrDropLeadAndTrailWhitespaces,
-  getStrConsecutiveWhitespacesToOnlyOne,
-  getStrLowercase,
-  getStrReplaceNonAscii,
-  getStrDropCharactersInBrackets,
-  getStrDropUndesiredCharacters,
-  getStrReplaceWhitespaces,
-};
+// Uncomment for testing.
+// export const exportedForTesting = {
+//   getStrModified,
+//   getStrDropLeadAndTrailWhitespaces,
+//   getStrConsecutiveWhitespacesToOnlyOne,
+//   getStrLowercase,
+//   getStrReplaceNonAscii,
+//   getStrDropCharactersInBrackets,
+//   getStrDropUndesiredCharacters,
+//   getStrReplaceWhitespaces,
+// };
