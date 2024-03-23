b8bcd3ad0533bc29bb6d579eb7877c5af4cd7ba9
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-23 18:28:09 +0100

    Uncomment no test code

--- src/modify-name.js
+++ src/modify-name.js
@@ -1,13 +1,13 @@
-//document.getElementById("search-button").onclick = function () {
-//  run_input_query();
-//};
-//
-//document.getElementById("search-input").onkeydown = function (e) {
-//  if (e.key === "Enter") {
-//    run_input_query();
-//  }
-//};
-//
+document.getElementById("search-button").onclick = function () {
+  run_input_query();
+};
+
+document.getElementById("search-input").onkeydown = function (e) {
+  if (e.key === "Enter") {
+    run_input_query();
+  }
+};
+
 function run_input_query() {
   const searchTerm = document.getElementById("search-input").value;
   try {
@@ -108,16 +108,16 @@ function getStrReplaceWhitespaces(string) {
 }
 
 // Uncomment for testing.
-export const exportedForTesting = {
-  getStrModified,
-  getStrDropLeadAndTrailWhitespaces,
-  getStrConsecutiveWhitespacesToOnlyOne,
-  getStrIssuerId,
-  getStrWithoutIssuerId,
-  getStrLowercase,
-  getStrReplaceNonAscii,
-  getStrDropCharactersInBrackets,
-  getStrIssuerRequiredPartsAsLowercase,
-  getStrDropUndesiredCharacters,
-  getStrReplaceWhitespaces,
-};
+// export const exportedForTesting = {
+//   getStrModified,
+//   getStrDropLeadAndTrailWhitespaces,
+//   getStrConsecutiveWhitespacesToOnlyOne,
+//   getStrIssuerId,
+//   getStrWithoutIssuerId,
+//   getStrLowercase,
+//   getStrReplaceNonAscii,
+//   getStrDropCharactersInBrackets,
+//   getStrIssuerRequiredPartsAsLowercase,
+//   getStrDropUndesiredCharacters,
+//   getStrReplaceWhitespaces,
+// };
